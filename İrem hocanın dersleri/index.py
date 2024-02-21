f0dcf43093e21f5f589134c4d2664914723a65c4
Ahmet Suat Tanış <ahmetsuattanis@gmail.com>
2024-02-21 17:00:33 +0300

    yeni dosyalama sistemim için commit

---
+++ İrem hocanın dersleri/index.py
@@ -0,0 +1,7 @@
+from classes2 import Student  #bizim projemiz
+#import random #python'daki hazır modül
+#import openpyxl #pip'den modül
+
+student1 = Student("İrem",2222)
+student1.notStudy()
+student1.doHomework()
